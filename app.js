67b9775f00f6b15c29c71180231dd1ac1ce8c728
Jorge Muñoz <mrjleo1989@gmail.com>
2017-06-30 12:48:14 -0500

    remaining files

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to database
+mongoose.connect(config.database);
+
+//On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database: ' + config.database);
+})
+
+//On error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+})
+
+const app = express();
+
+const users = require('./routes/users');
+
+//Port number
+const port = 3000;
+
+//cors middleware
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body-parser middleware
+app.use(bodyParser.json());
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+
+app.use('/users', users);
+
+//index route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'));
+})
+
+//app.use('/api',passport.authenticate('jwt', { session: false }) ,require('./routes/clienteApi'));
+
+//start server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+})
